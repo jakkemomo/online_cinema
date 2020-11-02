73550080c7d3dbfdca8438b74e4dd9325b7370c9
Alexander Olikov <alexolikov@gmail.com>
2020-11-02 19:18:34 +0300

    [FIX] Quotes changed to "

--- etl.py
+++ etl.py
@@ -3,7 +3,7 @@ import json
 import sqlite3
 from elasticsearch import Elasticsearch
 
-conn = sqlite3.connect('db.sqlite')
+conn = sqlite3.connect("db.sqlite")
 
 
 def fix_database():
@@ -28,31 +28,29 @@ def create_new_object(index, id, body):
 
 
 def get_writer_data(writer, writers):
+    def get_writer_ids(writer, writers):
+        if writers:
+            writer_ids = [writer.get("id") for writer in json.loads(writers)]
+        else:
+            writer_ids = [writer]
+        return writer_ids
     writer_ids = get_writer_ids(writer, writers)
     writers_sql_info = []
     for writer_id in set(writer_ids):
-        query = f"""select id, name from writers where writers.id = '{writer_id}'"""
+        query = f"select id, name from writers where writers.id = '{writer_id}'"
         writers_sql_info.extend(conn.execute(query).fetchall())
-    writer_names = [','.join([actor[1] for actor in writers_sql_info])]
+    writer_names = [",".join([actor[1] for actor in writers_sql_info])]
     writers_list = []
     for writer in writers_sql_info:
         writers_list.append(dict(id=writer[0], name=writer[1]))
     return writers_list, writer_names
 
 
-def get_writer_ids(writer, writers):
-    if writers:
-        writer_ids = [writer.get('id') for writer in json.loads(writers)]
-    else:
-        writer_ids = [writer]
-    return writer_ids
-
-
 def get_actor_data(movie_id):
     query = f"select actors.id, actors.name from movie_actors  inner join actors on actor_id=actors.id  where movie_id = '{movie_id}'"
     actors = conn.execute(query).fetchall()
     actor_list = []
-    actor_names = [','.join([actor[1] for actor in actors])]
+    actor_names = [",".join([actor[1] for actor in actors])]
     for actor in actors:
         actor_list.append(dict(id=actor[0], name=actor[1]))
     return actor_list, actor_names
@@ -68,13 +66,20 @@ def import_data():
         movie_id = movie[0]
         actor_data, actors_names = get_actor_data(movie_id)
         rating = float(movie[1]) if movie[1] and isinstance(movie[1], str) else None
-        movie_body = dict(id=movie_id, imdb_rating=rating, genre=movie[2], title=movie[3], description=movie[4],
-                          director=movie[5], actors_names=actors_names, writers_names=writers_names, actors=actor_data,
+        movie_body = dict(id=movie_id,
+                          imdb_rating=rating,
+                          genre=movie[2],
+                          title=movie[3],
+                          description=movie[4],
+                          director=movie[5],
+                          actors_names=actors_names,
+                          writers_names=writers_names,
+                          actors=actor_data,
                           writers=writer_data)
         create_new_object(index="movies", id=movie_id, body=movie_body)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fix_database()
     es = Elasticsearch()
     import_data()
--- movies_api/app.py
+++ movies_api/app.py
@@ -3,32 +3,32 @@ import requests
 
 from flask import Flask, request, jsonify
 
-app = Flask('movies_service')
+app = Flask("movies_service")
 
 
-@app.route('/api/movies/<movie_id>', methods=['GET'])
+@app.route("/api/movies/<movie_id>", methods=["GET"])
 def movie_details(movie_id: str) -> str:
     """
     :param movie_id: идентификатор фильма
     :return: данные из ES о фильме
     """
-    url = f'http://127.0.0.1:9200/movies/_doc/{movie_id}'
+    url = f"http://127.0.0.1:9200/movies/_doc/{movie_id}"
     response = requests.get(url)
-    json_data = json.loads(response.text).get('_source')
+    json_data = json.loads(response.text).get("_source")
     if json_data is not None:
         # Необходимые преобразования для прохождения теста в Postman
-        json_data['genre'] = json_data.get('genre', ',').split(', ')
-        json_data['director'] = json_data.get('director', ', ').split(', ')
+        json_data["genre"] = json_data.get("genre", ",").split(", ")
+        json_data["director"] = json_data.get("director", ", ").split(", ")
         # Необходимая сортировка для прохождения теста в Postman
-        json_data['writers'] = sorted(json_data['writers'], key=lambda i: i['name'].split(' ')[1])
-        data = dict(id=json_data.get('id'),
-                    title=json_data.get('title'),
-                    description=json_data.get('description'),
-                    imdb_rating=json_data.get('imdb_rating'),
-                    writers=json_data.get('writers'),
-                    actors=json_data.get('actors'),
-                    genre=json_data.get('genre'),
-                    director=json_data.get('director'))
+        json_data["writers"] = sorted(json_data["writers"], key=lambda i: i["name"].split(" ")[1])
+        data = dict(id=json_data.get("id"),
+                    title=json_data.get("title"),
+                    description=json_data.get("description"),
+                    imdb_rating=json_data.get("imdb_rating"),
+                    writers=json_data.get("writers"),
+                    actors=json_data.get("actors"),
+                    genre=json_data.get("genre"),
+                    director=json_data.get("director"))
         return jsonify(data)
     else:
         if response.status_code == 404:
@@ -37,34 +37,34 @@ def movie_details(movie_id: str) -> str:
             return "Error", response.status_code
 
 
-@app.route('/api/movies', methods=['GET'], strict_slashes=False)
+@app.route("/api/movies", methods=["GET"], strict_slashes=False)
 def movies_list() -> str:
     """
     :return: данные из ES об отфильтрованном по request.args списке фильмов
     """
     try:
-        search: str = request.args.get('search', '')
-        limit: int = int(request.args.get('limit', 50))
-        page: int = int(request.args.get('page', 1))
-        sort: str = request.args.get('sort', 'id')
-        sort_order: str = request.args.get('sort_order', 'asc')
+        search: str = request.args.get("search", "")
+        limit: int = int(request.args.get("limit", 50))
+        page: int = int(request.args.get("page", 1))
+        sort: str = request.args.get("sort", "id")
+        sort_order: str = request.args.get("sort_order", "asc")
     except Exception:
         return "Unprocessable Entity", 422
-    if limit <= 0 or page <= 0 or sort_order not in ['asc', 'desc'] or sort not in ['title', 'imdb_rating', 'id']:
+    if limit <= 0 or page <= 0 or sort_order not in ["asc", "desc"] or sort not in ["title", "imdb_rating", "id"]:
         return "Unprocessable Entity", 422
-    url = f'http://127.0.0.1:9200/movies/_search'
+    url = f"http://127.0.0.1:9200/movies/_search"
     from_value = page * limit - limit
 
     if search:
-        query = {'query': dict(multi_match=dict(query=search,
+        query = {"query": dict(multi_match=dict(query=search,
                                                 fields=["title^5", "description^4",
                                                         "genre^2",
                                                         "actors_names^3",
                                                         "writers_names",
                                                         "director"
                                                         ])),
-                 'size': limit,
-                 'from': from_value,
+                 "size": limit,
+                 "from": from_value,
                  "sort": [{sort: {"order": sort_order}}]}
     else:
         query = {
@@ -80,14 +80,14 @@ def movies_list() -> str:
     response = requests.post(
         url,
         data=json.dumps(query),
-        headers={'Content-Type': 'application/json'}
+        headers={"Content-Type": "application/json"}
     )
     if response.status_code == 200:
-        results = json.loads(response.text).get('hits', {}).get('hits', {})
+        results = json.loads(response.text).get("hits", {}).get("hits", {})
         data = []
         for result in results:
-            source_data = result.get('_source')
-            data.append(dict(id=source_data['id'], title=source_data['title'], imdb_rating=source_data['imdb_rating']))
+            source_data = result.get("_source")
+            data.append(dict(id=source_data["id"], title=source_data["title"], imdb_rating=source_data["imdb_rating"]))
         return jsonify(data)
     elif response.status_code == 400:
         return "Invalid request body format", 400
@@ -97,5 +97,5 @@ def movies_list() -> str:
         return "Link not found", 404
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=8000)
